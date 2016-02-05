06c1a3cafd321733e05c44ce6030d038bd4f02a5
Matt Layman <matthewlayman@gmail.com>
2016-02-04 23:12:44 -0500

    Arrange.

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,13 @@
 import unittest
 
+from calculator import Calculator
+
 
 class TestCalculator(unittest.TestCase):
 
     def test_adds_numbers(self):
-        pass
+        # Arrange
+        calculator = Calculator()
+
+        # Act
+        # Assert
